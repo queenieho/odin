3543413fd4ac6ada6a75c2fc1dce7311c104d20a
Josh Lehman <jalehman37@gmail.com>
2018-04-30 22:43:19 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -58,7 +58,7 @@
                  [cljsjs/moment "2.17.1-1"]
                  [akiroz.re-frame/storage "0.1.2"]
                  ;; DB
-                 [starcity/teller "1.0.2"]
+                 [starcity/teller "1.0.3"]
                  [starcity/blueprints "2.5.0"
                   :exclusions [com.datomic/datomic-free
                                com.andrewmcveigh/cljs-time
