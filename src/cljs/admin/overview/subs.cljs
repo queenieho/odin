cd7198f6c165084140f488c453b3f68463b5344d
Queenie Ho <kuini.ho@gmail.com>
2018-05-10 11:07:11 -0700

    implement new UI for notifications

--- src/cljs/admin/overview/subs.cljs
+++ src/cljs/admin/overview/subs.cljs
@@ -1,6 +1,7 @@
 (ns admin.overview.subs
   (:require [admin.overview.db]
-            [re-frame.core :refer [reg-sub]]))
+            [re-frame.core :refer [reg-sub]]
+            [iface.utils.time :as time]))
 
 
 ;; TODO filter out accounts with a move-out date
@@ -9,4 +10,19 @@
  :overview.accounts/end-of-term
  :<- [:accounts/list]
  (fn [accounts _]
-   accounts))
+   (->> (remove #(some? (get-in % [:active_license :move_out :date])) accounts)
+        #_(remove #(< 45 (time/days-between (get-in % [:active_license :ends])))))))
+
+
+(reg-sub
+ :overview.accounts/move-out
+ :<- [:accounts/list]
+ (fn [accounts _]
+   (let [accounts' (map
+                    #(if (even? (count (:name %)))
+                       (assoc-in % [:active_license :move_out] {:date     #inst "2018-06-06"
+                                                                :pre_walk #inst "2018-05-20"})
+                       %)
+                    accounts)]
+     accounts
+     #_(filter #(some? (get-in % [:active_license :move_out :date])) accounts))))
--- src/cljs/admin/overview/views.cljs
+++ src/cljs/admin/overview/views.cljs
@@ -12,7 +12,8 @@
             [iface.utils.time :as time]
             [re-frame.core :refer [subscribe dispatch]]
             [reagent.core :as r]
-            [toolbelt.core :as tb]))
+            [toolbelt.core :as tb]
+            [iface.utils.formatters :as formatters]))
 
 
 (def render-name-from-account
@@ -124,26 +125,78 @@
     :render render-days-left}])
 
 
+(defmulti notification (fn [type] type))
+
+
+(defmethod notification :orders [_ {:keys [account name status created] :as order}]
+  [:div.columns
+   [:div.column.is-1
+    [ant/avatar (formatters/initials (:name account))]]
+   [:div.column.is-11
+    [:p [:b (:name account)] " ordered "
+     [:b name]]
+    [:p.fs1 (format/date-short created) " - " status]]])
+
+
+(defmethod notification :payments [_ {:keys [amount description account status] :as payment}]
+  [:div.columns
+   [:div.column.is-1
+    [ant/avatar (formatters/initials (:name account))]]
+   [:div.column.is-11
+    [:p "Payment of " [:b amount] " for "
+     [:b description] " is " [:b status]]
+    [:p.fs1 (:name account)]]])
+
+
+(defmethod notification :end-of-term [_ {:keys [property name active_license] :as member}]
+  [:div.columns
+   [:div.column.is-1
+    [ant/avatar (formatters/initials name)]]
+   [:div.column.is-11
+    [:p [:b name] " is " [:b (time/days-between (:ends active_license)) " days"] " from end of term"]
+    [:p.fs1 (:name property) " - End of term: " (format/date-short (:ends active_license))]]])
+
+
+(defmethod notification :move-out [_ {:keys [property name active_license] :as member}]
+  (let [{[date pre_walk] :move_out} active_license]
+    [:div.columns
+     [:div.column.is-1
+      [ant/avatar (formatters/initials name)]]
+     [:div.column.is-11
+      [:p [:b name] " is moving out on " [:b (format/date-short (get-in active_license [:move_out :date]))]]
+      [:p.fs1 (:name property) " - Pre-walkthrough " (format/date-short (get-in active_license [:move_out :pre_walk]))]]]))
+
+
 (defn- notifications-table [type items title]
-  [:div.column.is-half
-   [:h3 title]
-   [ant/table {:pagination {:page-size 5
-                            :size      :small}
-               :columns    (columns type)
-               :dataSource (map #(assoc % :key (:id %)) items)}]])
+  (let [state (r/atom {:current 1})]
+    (fn [type items]
+      (let [{:keys [current]} @state
+            items'            (->> (drop (* (dec current) 5) items)
+                                   (take 5))]
+        [:div.column.is-half
+         [ant/card
+          {:title title}
+          (map #(with-meta [notification type %] {:key (:id %)}) items')
+          [ant/pagination {:page-size 5
+                           :size      :small
+                           :current   current
+                           :total     (count items)
+                           :on-change #(swap! state assoc :current %)}]]]))))
 
 
 (defn notifications []
   (let [payments (subscribe [:payments/by-status [:due :pending :failed]])
         orders   (subscribe [:orders])
-        members  (subscribe [:overview.accounts/end-of-term])]
+        members  (subscribe [:overview.accounts/end-of-term])
+        move-out (subscribe [:overview.accounts/move-out])]
     [:div
-     (.log js/console @members)
+     (.log js/console @move-out)
      [:div.columns
       [notifications-table :orders (sort-by :created > @orders) "Helping Hands"]
       [notifications-table :payments (sort-by :created > @payments) "Payments"]]
      [:div.columns
-      [notifications-table :end-of-term @members "End of term"]]]))
+      [notifications-table :end-of-term @members "End of term"]
+      [notifications-table :move-out @move-out "Moving out"]]]))
 
 
 (defn overview-content []
