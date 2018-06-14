bac2dea40e1e2768063f73e304aed3658b2a77c0
Josh Lehman <jalehman37@gmail.com>
2018-06-14 15:13:37 -0700

    bump blueprints

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
                  [akiroz.re-frame/storage "0.1.2"]
                  ;; DB
                  [starcity/teller "1.2.3-SNAPSHOT"]
-                 [starcity/blueprints "2.6.0"
+                 [starcity/blueprints "2.6.2"
                   :exclusions [com.datomic/datomic-free
                                com.andrewmcveigh/cljs-time
                                com.google.guava/guava
