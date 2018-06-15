ff5b68558eadcbd6e56ce2c26970782df289fdf5
Josh Lehman <jalehman37@gmail.com>
2018-06-14 18:38:41 -0700

    bump teller

--- project.clj
+++ project.clj
@@ -58,7 +58,7 @@
                  [cljsjs/moment "2.17.1-1"]
                  [akiroz.re-frame/storage "0.1.2"]
                  ;; DB
-                 [starcity/teller "1.2.3"]
+                 [starcity/teller "1.2.4"]
                  [starcity/blueprints "2.6.2"
                   :exclusions [com.datomic/datomic-free
                                com.andrewmcveigh/cljs-time
