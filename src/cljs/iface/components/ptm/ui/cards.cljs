c64a755b4cc002d8ad69d83f64a22f56707be493
Queenie Ho <kuini.ho@gmail.com>
2018-06-22 14:49:59 -0700

    add carousel to cards

--- src/cljs/iface/components/ptm/ui/cards.cljs
+++ src/cljs/iface/components/ptm/ui/cards.cljs
@@ -89,6 +89,93 @@
                :else                 [:a.text-link "Select"])})])
 
 
+(defn- carousel-next [{on-click :on-click}]
+  [:span.chevron.next
+   {:on-click #(on-click)}
+   [:img {:src "/assets/images/ptm/chevron.svg"}]])
+
+
+(defn- carousel-back [{on-click :on-click}]
+  [:span.chevron.back
+   {:on-click #(on-click)}
+   [:img {:src "/assets/images/ptm/chevron.svg"}]])
+
+
+(defn- carousel-dot [{:keys [active]}]
+  [:li.dot {:class (when-not active "inactive")}])
+
+
+(defn- carousel-slide [{:keys [selected img key]}]
+  [:li.carousel-slide
+   {:class (when selected
+             "active")
+    :key   key}
+   [:img {:src img}]])
+
+
+(defn- idx-next [idx total]
+  (if (= idx (- total 1)) 0 (inc idx)))
+
+
+(defn- idx-back [idx total]
+  (if (zero? idx) (dec total) (dec idx)))
+
+
+(defn- carousel [images]
+  (let [index (r/atom 0)]
+    (fn []
+      [:div.card-photo.aspect-ratio--6x4
+       {:style {:overflow   "hidden"}}
+       [:ul.dots
+        (doall
+         (map-indexed
+          #(with-meta
+             [carousel-dot
+              {:active (when (= %1 @index) true)}]
+             {:key %1})
+          images))]
+       [carousel-next {:on-click #(swap! index idx-next (count images))}]
+       [carousel-back {:on-click #(swap! index idx-back (count images))}]
+       [:div.chevron-scrim]
+       [:ul
+        (doall
+         (map-indexed
+          (fn [idx image]
+            [carousel-slide {:img      image
+                             :selected (when (= idx @index) true)
+                             :key      idx}])
+          images))]])))
+
+
+(defn carousel-card [{:keys  [value tag title subtitle description align
+                              on-click footer width disabled images]
+                      :or {align :center
+                           width :third}}]
+  [:div
+   {:class (get-card-width width)}
+   [:div.card
+    {:value value
+     :class (str
+             (when-not disabled
+               "card-interactive")
+             (get-card-align align))}
+    [carousel images]
+    [:div
+     {:on-click (when-let [c on-click]
+                  #(c value))}
+     (when tag
+       [:h4.top-0.right-1.absolute
+        [:div.pill tag]])
+     (when (or title subtitle description)
+       [:div.card-description
+        (when title [:h3.ma0 title])
+        (when subtitle [:h4.ma0 subtitle])
+        (when description [:p.mb0.mt3 description])])
+     (when footer
+       [:div.card-footer
+        footer])]]])
+
+
 (def card-count
   {:half  2
    :third 3})
