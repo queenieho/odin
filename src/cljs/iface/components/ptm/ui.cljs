936a68fd7ddade8dd2d3ecf1d95db13bfedffb23
Queenie Ho <kuini.ho@gmail.com>
2018-06-25 16:52:51 -0700

    removing vestigial code

--- src/cljs/iface/components/ptm/ui.cljs
+++ src/cljs/iface/components/ptm/ui.cljs
@@ -4,37 +4,6 @@
             [cljs.spec.alpha :as s]))
 
 
-
-;; button =======================================================================
-
-(defn- get-button-class [type]
-  (get {:primary ""
-        :secondary   "button-secondary"
-        :text        "button-text"} type))
-
-
-(s/def ::disabled
-  boolean?)
-
-(s/def ::type
-  #{:primary :secondary :text :upload})
-
-#_(defn button [{:keys [disabled type] :as props}]
-  (let [props' (-> props
-                   (merge {:class (str
-                                   (:class props) " "
-                                   (when (some? type) (get-button-class type))
-                                   (when disabled " button-disabled"))})
-                   (dissoc :disabled :type))]
-    (into [:button.button props']
-         (r/children (r/current-component)))))
-
-
-(s/fdef button
-        :args (s/cat :props (s/keys :opt-un [::disabled
-                                             ::type])))
-
-
 (defn pill [{:keys [active on-change on-delete selectable]}]
   (into [:div.pill {:class (str
                             (when active "pill-active")
