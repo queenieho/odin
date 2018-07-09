51f2efe7f6ba45f6b66c8c5b33c146e3df80142d
Queenie Ho <kuini.ho@gmail.com>
2018-07-09 14:00:10 -0700

    Add selected date to headline of step

--- src/cljs/apply/sections/logistics/get_notified.cljs
+++ src/cljs/apply/sections/logistics/get_notified.cljs
@@ -1,11 +1,12 @@
 (ns apply.sections.logistics.get-notified
   (:require [apply.content :as content]
             [antizer.reagent :as ant]
-            [re-frame.core :refer [dispatch subscribe]]
+            [re-frame.core :refer [dispatch subscribe reg-sub]]
             [apply.events :as events]
             [apply.db :as db]
             [iface.components.ptm.ui.button :as button]
-            [apply.routes :as routes]))
+            [apply.routes :as routes]
+            [iface.utils.formatters :as format]))
 
 
 (def step :logistics.move-in-date/get-notified)
@@ -19,19 +20,29 @@
   false)
 
 
+;; subs =========================================================================
+
+
+(reg-sub
+ :db
+ (fn [db _]
+   db))
+
 ;; views ========================================================================
 
 
-;; TODO get date for 45 days out
+;; NOTE I'm assuming that the date used here is for the user's
+;; selected date. If not, we have to revise this header
 (defmethod content/view step
   [_]
-  [:div
-   [:div.w-60-l.w-100
-    [:h1 "We'll send you an email notification before [date] to remind  you to apply."]
-    [:p "Thanks for your interest in Starcity! Hope to see you soon."]]
-   [:div.w-80-l.w-100
-    [:div.page-content
-     [:a {:href "/logout"}
-      [button/primary
-       {:on-click #()}
-       "Close"]]]]])
+  (let [date (subscribe [:step/data])]
+    [:div
+     [:div.w-60-l.w-100
+      [:h1 "We'll send you an email notification before " (format/date-month-day @date) " to remind  you to apply."]
+      [:p "Thanks for your interest in Starcity! Hope to see you soon."]]
+     [:div.w-80-l.w-100
+      [:div.page-content
+       [:a {:href "/logout"}
+        [button/primary
+         {:on-click #()}
+         "Close"]]]]]))
