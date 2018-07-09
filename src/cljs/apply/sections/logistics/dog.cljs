ecff96d49485c011f455df1710f1d3aecfa56129
Queenie Ho <kuini.ho@gmail.com>
2018-07-09 16:13:43 -0700

    Style layout of logistics.pets/dog form

--- src/cljs/apply/sections/logistics/dog.cljs
+++ src/cljs/apply/sections/logistics/dog.cljs
@@ -70,60 +70,68 @@
 (defmethod content/view step
   [_]
   (let [pupper (subscribe [::dog-form])]
-   [:div
-    [:div.w-60-l.w-100
-     [:h1 "Tell us about your fur family."]
-     [:p "Most of our communities are dog-friendly, but we unfortunately do not
+    [:div
+     [:div.w-60-l.w-100
+      [:h1 "Tell us about your fur family."]
+      [:p "Most of our communities are dog-friendly, but we unfortunately do not
     allow cats. If you have a dog, please let us know what breed and weight."]]
 
 
-    ;;NOTE - we need to style the form input components in such a way that we can have them inline.
-    [:div.page-content.w-60-l.w-100
-     [:div.cf.mb3-ns.mb0
-      [:div.w-40-l.w-100.fl-pr4-l.pr0
-       [form/item
-        {:label "Name"}
-        [form/text
-         {:on-change #(dispatch [::update-dog :name (.. % -target -value)])}]]]
-      [:div.w-40-l.w-100.fl-pr4-l.pr0
-       [form/item
-        {:label     "Breed"}
-        [form/text
-         {:on-change #(dispatch [::update-dog :breed (.. % -target -value)])}]]]
-      [:div.w-20-l.w-100.fl-pr4-l.pr0
-       [form/item
-        {:label "Weight"}
-        [form/number
-         {:placeholder "lbs"
-          :on-change #(dispatch [::update-dog :weight (.. % -target -value)])}]]]]
-     [form/form
-      [form/item
-       {:label "How will your dog be taken care of during the day?"}
-       [form/textarea
-        {:on-change #(dispatch [::update-dog :daytime_care (.. % -target -value)])}]]
-      [form/item
-       {:label "Please describe your dog's demeanor."}
-       [form/textarea
-        {:on-change #(dispatch [::update-dog :demeanor (.. % -target -value)])}]]
+     ;;NOTE - we need to style the form input components in such a way that we can have them inline.
+     [:div.w-60-l.w-100
+      [:div.page-content
+       [:div.cf.mb3-ns.mb0
+        [:div.w-40-l.w-100.fl.pr4-l.pr0
+         [form/item
+          {:label "Name"}
+          [form/text
+           {:on-change #(dispatch [::update-dog :name (.. % -target -value)])}]]]
+        [:div.w-40-l.w-100.fl.pr4-l.pr0
+         [form/item
+          {:label "Breed"}
+          [form/text
+           {:on-change #(dispatch [::update-dog :breed (.. % -target -value)])}]]]
+        [:div.w-20-l.w-100.fl.pr0
+         [form/item
+          {:label "Weight"}
+          [form/number
+           {:placeholder "lbs"
+            :on-change   #(dispatch [::update-dog :weight (.. % -target -value)])}]]]]
+       [:div.cf.mb3-ns.mb0
+        [form/item
+         {:label "How will your dog be taken care of during the day?"}
+         [form/textarea
+          {:on-change #(dispatch [::update-dog :daytime_care (.. % -target -value)])
+           :rows      3}]]]
+       [:div.cf.mb3-ns.mb0
+        [form/item
+         {:label "Please describe your dog's demeanor."}
+         [form/textarea
+          {:on-change #(dispatch [::update-dog :demeanor (.. % -target -value)])
+           :rows      3}]]]
 
-      ;; NOTE - we'll also need an inline-friendly radio-group variant
-      [form/item
-       {:label "Spayed/neutered?"}
-       [form/radio-group
-        {:name "spayed-neutered"}
-        [form/radio-option {} "Yes"]
-        [form/radio-option {} "No"]]]
+       ;; NOTE - we'll also need an inline-friendly radio-group variant
+       [:div.cf
+        [:div.w-third-l.w-100.fl.pr4-l.pr0.mb3
+         [form/item
+          {:label "Spayed/neutered?"}
+          [form/radio-group
+           {:name "spayed-neutered"}
+           [form/radio-option {} "Yes"]
+           [form/radio-option {} "No"]]]]
 
-      [form/item
-       {:label "Up-to-date vaccines?"}
-       [form/radio-group
-        {:name "vaccines"}
-        [form/radio-option {} "Yes"]
-        [form/radio-option {} "No"]]
+        [:div.w-third-l.w-100.fl.pr4-l.pr0.mb3
+         [form/item
+          {:label "Up-to-date vaccines?"}
+          [form/radio-group
+           {:name "vaccines"}
+           [form/radio-option {} "Yes"]
+           [form/radio-option {} "No"]]]]
 
-       [form/item
-        {:label "Has your dog ever bitten a human?"}
-        [form/radio-group
-         {:name "bitey"}
-         [form/radio-option {} "Yes"]
-         [form/radio-option {} "No"]]]]]]]))
+        [:div.w-third-l.w-100.fl.pr4-l.pr0.mb3
+         [form/item
+          {:label "Ever bitten a human?"}
+          [form/radio-group
+           {:name "bitey"}
+           [form/radio-option {} "Yes"]
+           [form/radio-option {} "No"]]]]]]]]))
