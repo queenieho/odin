954d37c37566f1dfbd954b801533e6e1e595f7bf
Queenie Ho <kuini.ho@gmail.com>
2018-07-09 13:46:56 -0700

    Add button to logistics:get-notified

--- src/cljs/apply/sections/logistics/get_notified.cljs
+++ src/cljs/apply/sections/logistics/get_notified.cljs
@@ -3,7 +3,9 @@
             [antizer.reagent :as ant]
             [re-frame.core :refer [dispatch subscribe]]
             [apply.events :as events]
-            [apply.db :as db]))
+            [apply.db :as db]
+            [iface.components.ptm.ui.button :as button]
+            [apply.routes :as routes]))
 
 
 (def step :logistics.move-in-date/get-notified)
@@ -20,11 +22,16 @@
 ;; views ========================================================================
 
 
+;; TODO get date for 45 days out
 (defmethod content/view step
   [_]
   [:div
    [:div.w-60-l.w-100
     [:h1 "We'll send you an email notification before [date] to remind  you to apply."]
     [:p "Thanks for your interest in Starcity! Hope to see you soon."]]
-   [:div.page-content.w-90-l.w-100
-    [:a {:href "/logout"}[ant/button "close"]]]])
+   [:div.w-80-l.w-100
+    [:div.page-content
+     [:a {:href "/logout"}
+      [button/primary
+       {:on-click #()}
+       "Close"]]]]])
