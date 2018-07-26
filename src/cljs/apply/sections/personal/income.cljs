1d1b37e61ad411a7a413205dbed5fdb23173149f
Queenie Ho <kuini.ho@gmail.com>
2018-07-26 11:02:57 -0700

    Make list pretty

--- src/cljs/apply/sections/personal/income.cljs
+++ src/cljs/apply/sections/personal/income.cljs
@@ -54,25 +54,41 @@
 ;; views ========================================================================
 
 
+(defn bullet-item
+  [valid label]
+  (let [icon-props {:type  (if valid "check" "close")
+                    :class (if valid "text-green pr2" "text-red pr2")
+                    :style {:font-size "2rem"}}]
+    [:div.pv2
+     {:display "table"}
+     [:span {:style {:display        "table-cell"
+                     :vertical-align "middle"}}
+      [ant/icon icon-props]]
+     [:span {:style {:display        "table-cell"
+                     :vertical-align "middle"}}
+      [:p label]]]))
+
+
 (defmethod content/view step
   [_]
   [:div
    [:div.w-60-l.w-100
     [:h1 "Please verify your income."]
     [:p "To qualify to live in Starcity, your gross income must be at least 2.5x the cost of rent. Please submit acceptable forms of verification."]]
-   [:div.page-content.w-90-l.w-100
-    [:p
-     [:ul
-      [:li "YES: Most recent pay stub"]
-      [:li "YES: Last three months bank statements"]
-      [:li "YES: Offer letter"]
-      [:li "NO: Stock portfolio"]
-      [:li "NO: Photo of your crypto wallet"]
-      [:li "NO: Photo of your actual wallet"]]]
+   [:div.w-60-l.w-100
+    [:div.page-content
+     [:div.w-50-l.w-100.fl.pl4-l.pl2
+      [bullet-item true "Most recent pay stub"]
+      [bullet-item true "Last three months' bank statements"]
+      [bullet-item true "Offer letter"]]
+     [:div.w-50-l.w-100.fl.pr4-l.pl2
+      [bullet-item false "Stock portfolio"]
+      [bullet-item false "Photo of your crypto wallet"]
+      [bullet-item false "Photo of your actual wallet"]]
 
-    [ant/button {:class "mt3"} "Upload files"]
+     [ant/button {:class "mt3"} "Upload files"]
 
-    [:p.mt3.mb3 "Are you taking a picture with your phone? Get an SMS link to finish this part of the application on your phone."]
+     [:p.mt3.mb3 "Are you taking a picture with your phone? Get an SMS link to finish this part of the application on your phone."]
 
-    [:span {:on-click #(dispatch [:step.current/next :cosigner])}
-     [form/checkbox {} "I am applying with a cosigner (i)"]]]])
+     [:span {:on-click #(dispatch [:step.current/next :cosigner])}
+      [form/checkbox {} "I am applying with a cosigner (i)"]]]]])
