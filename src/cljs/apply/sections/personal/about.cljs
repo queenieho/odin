acfd212f66df32d662d6a9109f524473e06dcb91
Queenie Ho <kuini.ho@gmail.com>
2018-07-30 10:32:15 -0700

    Make step: personal/about look pretty

--- src/cljs/apply/sections/personal/about.cljs
+++ src/cljs/apply/sections/personal/about.cljs
@@ -1,9 +1,10 @@
 (ns apply.sections.personal.about
   (:require [apply.content :as content]
-            [antizer.reagent :as ant]
-            [re-frame.core :refer [dispatch subscribe]]
-            [apply.events :as events]
             [apply.db :as db]
+            [apply.events :as events]
+            [antizer.reagent :as ant]
+            [clojure.string :as s]
+            [re-frame.core :refer [dispatch subscribe reg-event-fx]]
             [iface.components.ptm.ui.form :as form]
             [iface.utils.log :as log]))
 
@@ -38,10 +39,16 @@
 
 (defmethod db/step-complete? step
   [db step]
-  false)
+  (not (and (some? (step db)) (not (s/blank? (step db))))))
 
 
 ;; events =======================================================================
+
+
+(reg-event-fx
+ ::update-personal-about
+ (fn [{db :db} [_ v]]
+   {:db (assoc db step v)}))
 
 
 (defmethod events/save-step-fx step
@@ -55,25 +62,27 @@
 
 (defmethod content/view step
   [_]
-  [:div
-   [:div.w-60-l.w-100
-    [:h1 "Here's a little about us."]
-    ]
-   [:div.page-content.w-60-l.w-100
-    [:p.mb4 "We believe that community is best created by the connections formed
+  (let [about (subscribe [:db/step step])]
+    [:div
+     [:div.w-75-l.w-100
+      [:h1 "Here's a little about us."]
+      [:p.mb4 "We believe that community is best created by the connections formed
     between individuals through shared values. Read more about Our Values on our
     website."]
-    [:p.mb4 "Starcity is a safe space for individuals to come together and share
+      [:p.mb4 "Starcity is a safe space for individuals to come together and share
     their skills, experiences and perspectives. Our members give to one another
     and to the greater communities in which they live. We hope you're as excited
     about sharing and giving back as we are."]
-    [:p.mb4 "Lasting relationships are often built through common interests."]
-    [form/textarea
-     {:on-change #(log/log (.. % -target -value))
-      :placeholder "Help us get to know  you by telling us more about yourself. \n Where are you from? \n What do you like to do? \n What are your hopes and dreams?"}]
+      [:p.mb4 "Lasting relationships are often built through common interests."]
+      [form/textarea
+       {:on-change   #(dispatch [::update-personal-about (.. % -target -value)])
+        :value       @about
+        :placeholder "Help us get to know  you by telling us more about yourself. \n\nWhere are you from? \nWhat do you like to do? \nWhat are your hopes and dreams?"
+        :rows        8}]
 
-    [form/select
-     {:placeholder "How did you hear about us?"}
-     [form/select-option "Website"]
-     [form/select-option "Craigslist Ad"]
-     [form/select-option "Friend"]]]])
+      ;; NOTE will implement this after MVP for onboarding is ready
+      #_[form/select
+         {:placeholder "How did you hear about us?"}
+         [form/select-option "Website"]
+         [form/select-option "Craigslist Ad"]
+         [form/select-option "Friend"]]]]))
