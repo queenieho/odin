7bca9cf866cbe3dd3b9890028234377e00ebaa58
Queenie Ho <kuini.ho@gmail.com>
2018-08-08 10:56:50 -0700

    Remove usage of mod

--- src/cljs/apply/sections/personal/background_check_info.cljs
+++ src/cljs/apply/sections/personal/background_check_info.cljs
@@ -76,10 +76,10 @@
 
 (defn- is-leap?
   [year]
-  (cond (zero? (mod year 400)) true
-        (zero? (mod year 100)) false
-        (zero? (mod year 4))   true
-        :default               false))
+  (cond (zero? (rem year 400)) true
+        (zero? (rem year 100)) false
+        (zero? (rem year 4))   true
+        :else                  false))
 
 
 (defn- days-in-month
